2024a45b08d99b027d7eb72408685e7c11619fc9
Endre Synnes <endre.m.synnes@gmail.com>
2021-07-16 13:10:20 +0200

    small cleanup

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
 module "queue" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "~> 3.1"
+
+  name = var.queue_name
+  fifo_queue = var.is_fifo_queue
+  kms_master_key_id = var.kms_key
+
   redrive_policy = jsonencode({
     deadLetterTargetArn = module.dead_letter_queue.sqs_queue_arn,
     maxReceiveCount = 5
   })
-  name = var.queue_name
-  fifo_queue = var.is_fifo_queue
-  kms_master_key_id = var.kms_key
 }
 
 module "dead_letter_queue" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "~> 3.1"
+
   name = var.dead_letter_queue_name
   fifo_queue = var.is_fifo_queue
   kms_master_key_id = var.kms_key
